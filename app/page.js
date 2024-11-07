1b677aaff674e69d36e7e1e7eed4bff864269bd2
zacharysutton-bg <108938149+zacharysutton-bg@users.noreply.github.com>
2024-11-07 16:35:04 +0000

    mock timeline

--- app/page.js
+++ app/page.js
@@ -1,101 +1,122 @@
-import Image from "next/image";
+'use client';
+
+import { Calendar, Camera } from 'lucide-react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <VerticalTimeline lineColor='lightgray' layout='2-columns'>
+      <VerticalTimelineElement
+        className='vertical-timeline-element--work'
+        contentStyle={{ background: 'lightgray', color: '#000' }}
+        contentArrowStyle={{ borderRight: '7px solid  lightgray' }}
+        date='1825'
+        iconStyle={{ background: 'rgb(255, 0, 0)', color: '#fff' }}
+        icon={<Calendar />}
+      >
+        <h3 className='vertical-timeline-element-title text-4xl'>1825</h3>
+        <p>
+          German Carl Voelker sets up an outdoor gymnastic school near Regents Park London. Unfortunately, it does not survive beyond 1830, but the later formed
+          Primrose Hill Gymnasium supported by the Ministry of Parks has better success.
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className='vertical-timeline-element--work'
+        contentStyle={{ background: 'lightgray', color: '#000' }}
+        contentArrowStyle={{ borderRight: '7px solid  lightgray' }}
+        date='1838'
+        iconStyle={{ background: 'rgb(255, 0, 0)', color: '#fff' }}
+        icon={<Camera />}
+      >
+        <h3 className='vertical-timeline-element-title text-4xl'>1825</h3>
+        <p>
+          German Carl Voelker sets up an outdoor gymnastic school near Regents Park London. Unfortunately, it does not survive beyond 1830, but the later formed
+          Primrose Hill Gymnasium supported by the Ministry of Parks has better success.
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className='vertical-timeline-element--work'
+        contentStyle={{ background: 'lightgray', color: '#000' }}
+        contentArrowStyle={{ borderRight: '7px solid  lightgray' }}
+        date='2008 - 2010'
+        iconStyle={{ background: 'rgb(255, 0, 0)', color: '#fff' }}
+        icon={<Camera />}
+      >
+        <h3 className='vertical-timeline-element-title text-4xl'>1825</h3>
+        <p>
+          German Carl Voelker sets up an outdoor gymnastic school near Regents Park London. Unfortunately, it does not survive beyond 1830, but the later formed
+          Primrose Hill Gymnasium supported by the Ministry of Parks has better success.
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className='vertical-timeline-element--work'
+        contentStyle={{ background: 'lightgray', color: '#000' }}
+        contentArrowStyle={{ borderRight: '7px solid  lightgray' }}
+        date='2006 - 2008'
+        iconStyle={{ background: 'rgb(255, 0, 0)', color: '#fff' }}
+        icon={<Camera />}
+      >
+        <h3 className='vertical-timeline-element-title text-4xl'>1825</h3>
+        <p>
+          German Carl Voelker sets up an outdoor gymnastic school near Regents Park London. Unfortunately, it does not survive beyond 1830, but the later formed
+          Primrose Hill Gymnasium supported by the Ministry of Parks has better success.
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className='vertical-timeline-element--education'
+        contentStyle={{ background: 'lightgray', color: '#000' }}
+        contentArrowStyle={{ borderRight: '7px solid  lightgray' }}
+        date='April 2013'
+        iconStyle={{ background: 'rgb(255, 0, 0)', color: '#fff' }}
+        icon={<Calendar />}
+      >
+        <h3 className='vertical-timeline-element-title text-4xl'>1825</h3>
+        <p>
+          German Carl Voelker sets up an outdoor gymnastic school near Regents Park London. Unfortunately, it does not survive beyond 1830, but the later formed
+          Primrose Hill Gymnasium supported by the Ministry of Parks has better success.
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className='vertical-timeline-element--education'
+        contentStyle={{ background: 'lightgray', color: '#000' }}
+        contentArrowStyle={{ borderRight: '7px solid  lightgray' }}
+        date='November 2012'
+        iconStyle={{ background: 'rgb(255, 0, 0)', color: '#fff' }}
+        icon={<Calendar />}
+      >
+        <h3 className='vertical-timeline-element-title text-4xl'>1825</h3>
+        <p>
+          German Carl Voelker sets up an outdoor gymnastic school near Regents Park London. Unfortunately, it does not survive beyond 1830, but the later formed
+          Primrose Hill Gymnasium supported by the Ministry of Parks has better success.
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className='vertical-timeline-element--education'
+        contentStyle={{ background: 'lightgray', color: '#000' }}
+        contentArrowStyle={{ borderRight: '7px solid  lightgray' }}
+        date='2002 - 2006'
+        iconStyle={{ background: 'rgb(255, 0, 0)', color: '#fff' }}
+        icon={<Calendar />}
+      >
+        <h3 className='vertical-timeline-element-title text-4xl'>1825</h3>
+        <p>
+          German Carl Voelker sets up an outdoor gymnastic school near Regents Park London. Unfortunately, it does not survive beyond 1830, but the later formed
+          Primrose Hill Gymnasium supported by the Ministry of Parks has better success.
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        iconStyle={{ background: 'rgb(255, 0, 0)', color: '#fff' }}
+        contentStyle={{ background: 'lightgray', color: '#000' }}
+        contentArrowStyle={{ borderRight: '7px solid  lightgray' }}
+        icon={<Calendar />}
+      >
+        <h3 className='vertical-timeline-element-title text-4xl'>1825</h3>
+        <p>
+          German Carl Voelker sets up an outdoor gymnastic school near Regents Park London. Unfortunately, it does not survive beyond 1830, but the later formed
+          Primrose Hill Gymnasium supported by the Ministry of Parks has better success.
+        </p>
+      </VerticalTimelineElement>
+    </VerticalTimeline>
   );
 }
