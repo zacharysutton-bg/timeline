5da67786a99403774aa1a7ece20a4cf6eb504105
zacharysutton-bg <108938149+zacharysutton-bg@users.noreply.github.com>
2024-11-07 17:52:17 +0000

    head import fixed

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import localFont from 'next/font/local';
 import './globals.css';
-import { GoogleAnalytics } from '@next/third-parties/google';
+import Head from 'next/head';
 import Script from 'next/script';
 
 const geistSans = localFont({
